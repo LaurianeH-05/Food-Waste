f632681ed278d3a6879b337b240fbd7997a46708
Wandathomas2 <151261174+Wandathomas2@users.noreply.github.com>
2024-07-18 19:05:15 -0400

    Update foodWaste-SliderInformation.js

--- foodWaste-SliderInformation.js
+++ foodWaste-SliderInformation.js
@@ -3,12 +3,12 @@ const initSlider = () => {
   //slides images according to the buttons
     const imageList = document.querySelector(".slider-wrapper .image-list");
     const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
-    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth
+    const maxScrollLeft = imageList.scrollWidth - imageList.clientWidth;
     
     slideButtons.forEach(button => {
       button.addEventListener("click", () => {
         const direction = button.id === "backwards" ? -1 : 1;
-        const scrollAmount = imageList.clientWidth * direction
+        const scrollAmount = imageList.clientWidth * direction;
         imageList.scrollBy({left: scrollAmount,behavior: "smooth"});
     });
   });
@@ -16,6 +16,8 @@ const initSlider = () => {
     slideButtons[0].style.display = imageList.scrollLeft <= 0 ? "none" : "block";
     slideButtons[1].style.display = imageList.scrollLeft >=  maxScrollLeft ? "none" : "block";
   }
+
+
   
   imageList.addEventListener("scroll", () => {
     handleSlideButtons();
