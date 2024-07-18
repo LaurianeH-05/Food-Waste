026157a7909a3f5ba3280c892fa0829d8ae9f6b5
Lory <147001347+LaurianeH-05@users.noreply.github.com>
2024-07-18 19:00:32 -0400

    Update Maps.js

--- Maps.js
+++ Maps.js
@@ -1,8 +1,19 @@
-function toggleReview() {
-    var reviewsContainer = document.getElementById('reviewsContainer');
-    if (reviewsContainer.style.display === 'none') {
-        reviewsContainer.style.display = 'block';
-    } else {
-        reviewsContainer.style.display = 'none';
-    }
+function initMap() {
+    const center = { lat: -34.397, lng: 150.644};
+    const map = new
+    google.maps.Map(document.getElementById("map-container"), {
+        zoom: 8,
+        center: center,
+    });
 }
+
+function loadScript() {
+    const script =
+    document.createElement("script");
+    script.src = "API";
+    script.async = true;
+    script.defer = true;
+    document.head.appendChild(script);
+}
+
+window.onload = loadScript;
